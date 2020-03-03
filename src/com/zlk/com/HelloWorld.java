d3f7e09115945730a1e4caad99e060aa84fe0929
zlk <m17770879759@163.com>
2020-03-03 14:25:11 +0800

    Initial commit

---
+++ src/com/zlk/com/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.zlk.com;
+
+import com.sun.xml.internal.ws.message.stream.PayloadStreamReaderMessage;
+import java.sql.SQLOutput;
+
+/**
+ * @Author zlk
+ * @create 2020/3/3 11:07
+ */
+public class HelloWorld {
+//    程序执行的起点
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
+/*关键字
+1）完全小写纯英文字母：
+2）
+ */
+
+/*
+* 标识符命名规范
+*
+* 类名规范：首字母大写，后面每个单词首字母大写（大驼峰写法）
+* 变量名、方法名规范：首字母小写，后面每个单词字母大写（小驼峰写法）
+*
+*/
+
