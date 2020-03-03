cf479496234751c3b75c2b8c8863870cd3c75910
zlk <m17770879759@163.com>
2020-03-03 14:36:54 +0800

    注释内容

--- src/com/zlk/com/HelloWorld.java
+++ src/com/zlk/com/HelloWorld.java
@@ -1,11 +1,9 @@
 package com.zlk.com;
 
-import com.sun.xml.internal.ws.message.stream.PayloadStreamReaderMessage;
-import java.sql.SQLOutput;
 
 /**
- * @Author zlk
- * @create 2020/3/3 11:07
+ * @ zlk
+ * @ 2020/3/3 11:07
  */
 public class HelloWorld {
 //    程序执行的起点
@@ -13,6 +11,7 @@ public class HelloWorld {
         System.out.println("HelloWorld!");
     }
 }
+
 /*关键字
 
 1）完全小写纯英文字母：
